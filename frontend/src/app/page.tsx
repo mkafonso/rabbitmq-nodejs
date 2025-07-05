98e0c3bae6587d30729372ef4ca42dce2a423a39
mkafonso <mkafonso.dev@gmail.com>
2025-07-05 01:35:42 -0300

    feat: init frontend app

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <strong>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum nobis
+      consequuntur excepturi ipsa maxime, aut atque nihil dicta velit in quam
+      incidunt voluptatibus dolor dolores nemo quis quod, doloremque
+      exercitationem.
+    </strong>
+  );
+}
