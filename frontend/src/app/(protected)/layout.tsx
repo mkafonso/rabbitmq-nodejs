809ceae3694305dbe98a64d45e5f0c1f8ac15cbc
mkafonso <mkafonso.dev@gmail.com>
2025-07-05 11:32:38 -0300

    fix: redict users without authentication

--- frontend/src/app/(protected)/layout.tsx
+++ frontend/src/app/(protected)/layout.tsx
@@ -1,6 +1,15 @@
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 import { PropsWithChildren } from "react";
 
 export default async function ApplicationLayout(props: PropsWithChildren) {
   const { children } = props;
+  const cookieStore = await cookies();
+  const token = cookieStore.get("projeto:token");
+
+  if (!token) {
+    redirect("/entrar");
+  }
+
   return <div className="w-full h-full overflow-hidden">{children}</div>;
 }
